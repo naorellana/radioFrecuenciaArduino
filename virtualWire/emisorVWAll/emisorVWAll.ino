9557e94716f93a0433d0d65a641f216fd953272e
Nery Alexis Orellana <norellanac@miumg.edu.gt>
2019-10-24 17:54:01 -0600

    testing con boton para enviar datos

--- virtualWire/emisorVWAll/emisorVWAll.ino
+++ virtualWire/emisorVWAll/emisorVWAll.ino
@@ -1,17 +1,17 @@
-#include<Keypad.h>
-#include <VirtualWire.h>
+#include<Keypad.h> //libreria para usar el teclado
+#include <VirtualWire.h> //libreria para usar modulo 433
 
-const byte filas = 4;
-const byte columnas = 4;
-byte pinesF[filas] = {9, 11, 10, 8};
-byte pinesC[columnas] = {7, 6, 5, 4};
+const byte filas = 4; //filas del teclado
+const byte columnas = 4;//columnas del teclado
+byte pinesF[filas] = {9, 11, 10, 8};//conectar pines de las filas
+byte pinesC[columnas] = {7, 6, 5, 4}; //conectar pines de las columnas
 
 char teclas[filas][columnas] = {
 
   {'1', '2', '3', 'a'},
   {'4', '5', '6', 'b'},
   {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  {'*', '0', '#', 'd'}
 };
 
 Keypad teclado = Keypad(makeKeymap(teclas), pinesF, pinesC, filas, columnas);
@@ -19,54 +19,36 @@ Keypad teclado = Keypad(makeKeymap(teclas), pinesF, pinesC, filas, columnas);
 char tecla ;
 
 
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h> 
+#include <Wire.h> //libreria para pantalla lcd
+#include <LiquidCrystal_I2C.h> //libreria para pantalla lcd 
 // Constructor de la librería de LCD 16x2
 // Aqui se configuran los pines asignados a la pantalla del PCF8574
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
+const int button=13;
 
 void setup()
 {
   Serial.begin(9600);
-
+  pinMode(button, INPUT);
   // virtual wire
-  vw_set_tx_pin(12); // pin
+  vw_set_tx_pin(12); // pin para modulo emisor ATAD
   vw_setup(8000); // bps
 
   //pantalla
-  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
-  lcd.begin(16, 2);
-  // Mover el cursor a la primera posición de la pantalla (0, 0)
-  lcd.home ();
-  // Imprimir "Hola Mundo" en la primera linea
-  lcd.print("Proyecto Final");
-  // Mover el cursor a la segunda linea (1) primera columna
-  lcd.setCursor ( 0, 1 );
-  // Imprimir otra cadena en esta posicion
-  lcd.print("Telco 2019");
-  // Esperar un segundo
-  delay(1000);
+  printScreen("Proyecto Final", "Telco 2019");
 }
 
 void loop()
 {
+  if (digitalRead(button)==HIGH){
+    Serial.println("Alto");
+  }else{
+    Serial.println("BAJO");
+  }
   int i = 0;
   String digitos = "";
-  //pantalla
-  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
-  lcd.begin(16, 2);
-  // Mover el cursor a la primera posición de la pantalla (0, 0)
-  lcd.home ();
-  // Imprimir "Hola Mundo" en la primera linea
-  lcd.print("Preparado");
-  // Mover el cursor a la segunda linea (1) primera columna
-  lcd.setCursor ( 0, 1 );
-  // Imprimir otra cadena en esta posicion
-  lcd.print("Ingrese 2 digitos");
-  // Esperar un segundo
-  delay(1000);
-  Serial.println("INGRESE 2 digitos");
+  printScreen("Esperando datos...", "Ingresar 2 digitos");
+  Serial.println("Ingresar 2 digitos");
   while (i < 2) {
     if (tecla = teclado.getKey()) {
       i++;
@@ -79,19 +61,8 @@ void loop()
   sendString(digitos, true); 
   delay(100);
   
-  //pantalla
-  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
-  lcd.begin(16, 2);
-  // Mover el cursor a la primera posición de la pantalla (0, 0)
-  lcd.home ();
-  // Imprimir "Hola Mundo" en la primera linea
-  lcd.print("Enviando...");
-  // Mover el cursor a la segunda linea (1) primera columna
-  lcd.setCursor ( 0, 1 );
-  // Imprimir otra cadena en esta posicion
-  lcd.print(digitos);
-  // Esperar un segundo
-  delay(1000);
+  printScreen("Enviando...", digitos);
+  printScreen("Enviado!", digitos);
 }
 
 void sendString(String message, bool wait)
@@ -107,4 +78,19 @@ void sendString(String message, bool wait)
   if (wait) vw_wait_tx(); 
 
   Serial.println("Enviado: " + message); 
+}
+
+void printScreen(String text1, String text2){
+  //pantalla
+  // Indicar a la libreria que tenemos conectada una pantalla de 16x2
+  lcd.begin(16, 2);
+  // Mover el cursor a la primera posición de la pantalla (0, 0)
+  lcd.home ();
+  lcd.print(text1);
+  // Mover el cursor a la segunda linea (1) primera columna
+  lcd.setCursor ( 0, 1 );
+  // Imprimir otra cadena en esta posicion
+  lcd.print(text2);
+  // Esperar un segundo
+  delay(1000);
 }
